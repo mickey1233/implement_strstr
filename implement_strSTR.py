e87231642b46e99d764c2fff3f13700efdab1d25
mickey1233 <42233582+mickey1233@users.noreply.github.com>
2021-09-02 16:15:25 +0800

    Add files via upload

---
+++ implement_strSTR.py
@@ -0,0 +1,12 @@
+def strStr(haystack, needle):
+    print(haystack.find(needle))
+
+
+def main():
+    strStr("hello", "ll")
+    strStr("aaaaa", "bba")
+    strStr("", "")
+
+
+if __name__ == "__main__":
+    main()
